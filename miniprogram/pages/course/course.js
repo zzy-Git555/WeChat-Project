4ff23189fd6e0bf804303f2319e69620ea3dd97f
lala <1050123255@qq.com>
2020-04-27 21:42:59 +0800

    second commit

--- miniprogram/pages/course/course.js
+++ miniprogram/pages/course/course.js
@@ -6,13 +6,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    item: 0,
+    tab: 0
   },
-  detail: function (e) {
+  detail: function(e) {
     var data = JSON.stringify(e.currentTarget.dataset.result)
-    console.log("传输过去的数据:",data)
+    console.log("传输过去的数据:", data)
     wx.navigateTo({
-      url: '/pages/template/template?detail='+data,
+      url: '/pages/template/template?detail=' + data,
     })
   },
   /**
@@ -29,9 +30,36 @@ Page({
   //     }
   //   })
   // },
+  //页面切换
+  changeItem: function(e) {
+    console.log(e)
+    this.setData({
+      item: e.target.dataset.index
+    })
+  },
+  //tab切换
+  changeTab: function(e) {
+    console.log(e)
+    this.setData({
+      tab: e.detail.current
+    })
+  },
 
-  
   onLoad: function(options) {
+    var that = this
+    // wx.cloud.callFunction({
+    //   name: 'getContent',
+    //   success: res => {
+    //     console.log("云函数返回的数据", res)
+    //     var result = res.data
+    //     this.setData({
+    //       result
+    //     })
+    //   },
+    //   fail: err => {
+    //     console.error('云函数调用失败', err)
+    //   }
+    // })
     const db = wx.cloud.database()
     db.collection("Content").get()
 
@@ -45,8 +73,15 @@ Page({
       }).catch(err => {
         console.error(err)
       })
+    var that = this
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          clientHeight: res.windowHeight
+        });
+      }
+    })
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
